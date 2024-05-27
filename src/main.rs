f84e5841c9d3507d101dcf2b49ed93a99575d167
Kristian Quirapas <kmquirapas@gmail.com>
2024-05-27 22:45:06 +0800

    add: env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use colored::Colorize;
+use dotenv::dotenv;
+use std::env;
+
+// defaults
+const PORT_DEFAULT: &str = "8080";
+const DATABASE_NAME_DEFAULT: &str = "blocks.db";
+const BLOCK_TIME_DEFAULT: &str = "1"; // in seconds
 
 /// Program for producing blocks and persisting
 /// them into a database.
@@ -7,15 +15,31 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Args {
     /// port to use for block-factory (e.g. 8080)
-    #[arg(short, long)]
+    #[clap(short, long, default_value = PORT_DEFAULT)]
     port: Option<String>,
 
     /// name to use for your sqlite DB file name
-    #[arg(short, long)]
+    #[arg(short, long, default_value = DATABASE_NAME_DEFAULT)]
     database: Option<String>,
+
+    /// set how long (in seconds) the factory will wait before it
+    /// wrangles blocks in the pool and creates a block
+    #[arg(short, long, default_value = BLOCK_TIME_DEFAULT)]
+    block_time: Option<u32>,
 }
 
 fn main() {
+    dotenv().ok(); // Load the .env file
+
     let args = Args::parse();
-    println!("{:?}", args);
+    println!("{}", format!("{:?}", args).green().bold());
+
+    println!(
+        "{}",
+        env::var("BLOCK_TIME")
+            .unwrap_or(String::from(BLOCK_TIME_DEFAULT))
+            .to_string()
+            .yellow()
+            .bold()
+    );
 }
