f6d7cbf1718b8785df7b9c2b7247e18b882f5a34
Kristian Quirapas <kmquirapas@gmail.com>
2024-06-02 10:09:20 +0800

    add: task spawning

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ async fn main() -> Result<()> {
         .get_matches();
 
     // parse arguments and flags
-    let port = matches.get_one::<u32>("PORT").unwrap();
+    let port = *matches.get_one::<u32>("PORT").unwrap();
     let block_time = *matches.get_one::<u32>("BLOCKTIME").unwrap();
     let mode = matches.get_one::<Mode>("MODE").unwrap();
 
@@ -69,13 +69,17 @@ async fn main() -> Result<()> {
     }));
 
     // display config with beautiful table
-    utils::display_configuration(port, &block_time, mode);
+    utils::display_configuration(&port, &block_time, mode);
 
+    // run the node in a task
     let config = Arc::clone(&shared_config);
     let node_handle = tokio::spawn(async move {
+        println!("Spawning node runner...");
+        // get ownership of BLOCK TIME
+        let owned_block_time = block_time.to_owned();
         // run the node
         let config = config.lock().await;
-        config.node.run(block_time.to_owned()).await
+        config.node.run(owned_block_time).await
     });
 
     // get routes
@@ -88,11 +92,19 @@ async fn main() -> Result<()> {
     // add global 404
     let app = app.fallback(ui::not_found);
 
-    // run our app with hyper, listening globally on {--port}
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
+    // serve block factory in a task
+    let server_handle = tokio::spawn(async move {
+        println!("Spawning server...");
+        // get ownership of PORT
+        let owned_port = port.to_owned();
+        // run our app with hyper, listening globally on {--port}
+        let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{owned_port}")).await?;
+        // run the server
+        axum::serve(listener, app).await
+    });
 
-    // serve block factory
-    axum::serve(listener, app).await?;
+    node_handle.await??;
+    server_handle.await??;
 
     Ok(())
 }
